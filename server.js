ae971778f6f9dcae7e94049b35f0389df370e51d
Tasos Simitzis <tasossimitzis1@gmail.com>
2024-04-22 14:00:05 +0300

    mongodbFiles

--- server.js
+++ server.js
@@ -2,6 +2,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const { MongoClient } = require('mongodb');
+
+const uri = "mongodb+srv://tasgate13:9dFB5M0RvuEEsWQa@cluster0.hq75pvw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(uri);
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected to the MongoDB cluster");
+        // Προχώρησε με τις ενέργειες που θέλεις να κάνεις με τη βάση δεδομένων εδώ
+    } finally {
+        await client.close();
+    }
+}
+
+run().catch(console.error);
+
+
 const app = express();
 app.use(cors());
 const port = 3000;
@@ -25,7 +44,4 @@ app.post('/save-name', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
-
-
-
 
