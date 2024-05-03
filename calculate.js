f2f1678291c0dbef27ad8ec044e16c008d97a35d
Tasos Simitzis <tasossimitzis1@gmail.com>
2024-05-03 20:39:33 +0300

    calculate calories algorithm implementation

--- calculate.js
+++ calculate.js
@@ -4,9 +4,18 @@ function showInput() {
     const age = document.getElementById('age').value;
 
     const userInputElement = document.getElementById('userInput');
-    userInputElement.innerHTML = `<p>Weight: ${weight}</p><p>Height: ${height}</p><p>Age: ${age}</p>`;
+    userInputElement.innerHTML = `<h3>You Should Eat ${calculateDailyCalories("Male",weight,height,age)} Calories Per Day! </h3><h4>Weight: ${weight}</h4><h4>Height: ${height}</h4><h4>Age: ${age}</h4>`;
     console.log("All values ok");
 
     return false;
-    //thelei veltiwsi
+}
+
+function calculateDailyCalories(gender, weight,height,age){
+    let BMR;
+    if(gender=="Male"){
+        BMR = (10*weight) + (6.25*height) - (5*age) +5;
+    }else{
+        BMR = (10*weight) + (6.25*height) - (5*age) -161;
+    }
+    return BMR; //leitoyrgei kala apla na vrw pws na pairnw timi apo to radio button
 }
