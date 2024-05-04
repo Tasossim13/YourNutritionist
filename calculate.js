d4bf23e5ce8b6467c36fa411b37cdec447c8e5e0
Tasos Simitzis <tasossimitzis1@gmail.com>
2024-05-04 14:08:48 +0300

    calculate daily calories algorithm completed

--- calculate.js
+++ calculate.js
@@ -3,19 +3,32 @@ function showInput() {
     const height = document.getElementById('height').value;
     const age = document.getElementById('age').value;
 
-    const userInputElement = document.getElementById('userInput');
-    userInputElement.innerHTML = `<h3>You Should Eat ${calculateDailyCalories("Male",weight,height,age)} Calories Per Day! </h3><h4>Weight: ${weight}</h4><h4>Height: ${height}</h4><h4>Age: ${age}</h4>`;
-    console.log("All values ok");
+    let gender;
 
+    if (document.getElementById('maleId').checked) {
+        gender = document.getElementById('maleId').value;
+    } else if (document.getElementById('femaleId').checked) {
+        gender = document.getElementById('femaleId').value;
+    } else {
+        gender = "Not specified";
+    }
+
+    const userInputElement = document.getElementById('userInput');
+    if(gender=="male" || gender=="female"){
+        userInputElement.innerHTML = `<h3>You Should Eat ${calculateDailyCalories(gender,weight,height,age)} Calories Per Day! </h3><h4>Weight: ${weight}</h4><h4>Height: ${height}</h4><h4>Age: ${age}</h4>`;
+    }else{
+        userInputElement.innerHTML = `<h3>Gender Is Not Specified! </h3>`;
+    }
+    
     return false;
 }
 
 function calculateDailyCalories(gender, weight,height,age){
     let BMR;
-    if(gender=="Male"){
+    if(gender=="male"){
         BMR = (10*weight) + (6.25*height) - (5*age) +5;
     }else{
         BMR = (10*weight) + (6.25*height) - (5*age) -161;
     }
-    return BMR; //leitoyrgei kala apla na vrw pws na pairnw timi apo to radio button
+    return BMR; //leitoyrgei mia xara
 }
