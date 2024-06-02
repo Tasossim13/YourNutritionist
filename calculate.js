4c862d7037dc7b3ad0313562910696c5205c6d1e
Tasos Simitzis <tasossimitzis1@gmail.com>
2024-06-03 02:07:38 +0300

    calculate exercise add

--- calculate.js
+++ calculate.js
@@ -2,6 +2,7 @@ function showInput() {
     const weight = document.getElementById('weight').value;
     const height = document.getElementById('height').value;
     const age = document.getElementById('age').value;
+    const exercise = document.getElementById('exerciseId').value;
 
     let gender;
 
@@ -15,7 +16,7 @@ function showInput() {
 
     const userInputElement = document.getElementById('userInput');
     if(gender=="male" || gender=="female"){
-        userInputElement.innerHTML = `<h3>You Should Eat ${calculateDailyCalories(gender,weight,height,age)} Calories Per Day! </h3><h4>Weight: ${weight}</h4><h4>Height: ${height}</h4><h4>Age: ${age}</h4>`;
+        userInputElement.innerHTML = `<h3>You Should Eat ${calculateDailyCalories(gender,weight,height,age, exercise)} Calories Per Day! </h3><h4>Weight: ${weight}</h4><h4>Height: ${height}</h4><h4>Age: ${age}</h4>`;
     }else{
         userInputElement.innerHTML = `<h3>Gender Is Not Specified! </h3>`;
     }
@@ -23,7 +24,12 @@ function showInput() {
     return false;
 }
 
-function calculateDailyCalories(gender, weight,height,age){
+function calculateDailyCalories(gender, weight,height,age,exercise){
+    /*na ftiaksw Sedentary (little to no exercise): TDEE = BMR × 1.2
+Lightly Active (light exercise/sports 1-3 days/week): TDEE = BMR × 1.375
+Moderately Active (moderate exercise/sports 3-5 days/week): TDEE = BMR × 1.55
+Very Active (hard exercise/sports 6-7 days a week): TDEE = BMR × 1.725
+Super Active (very hard exercise/sports, physical job, or training): TDEE = BMR × 1.9*/
     let BMR;
     if(gender=="male"){
         BMR = (10*weight) + (6.25*height) - (5*age) +5;
